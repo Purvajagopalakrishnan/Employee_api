d31b26f1f61fc3378c6754d00702006348058c81
purvaja gopalakrishnan <purvajag@CESCHN.COM>
2019-01-16 18:41:41 +0530

    Employee api

---
+++ EmployeePortal/Controllers/CreateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeePortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    public class CreateController : Controller
+    {
+        List<Employee> emp = new List<Employee>()
+        {
+            new Employee{Id=1, Name= "Rahul", Designation="software eng",Exp=2},
+            new Employee{Id=2, Name= "Priya", Designation="senior software eng",Exp=5},
+            new Employee{Id=3, Name= "Anand", Designation="software eng",Exp=2},
+            new Employee{Id=4, Name= "Sowmiya", Designation="Technical lead",Exp=9},
+            new Employee{Id=5, Name= "John", Designation="software eng",Exp=1},
+        };
+        [Route("api")]
+        public ActionResult<Dictionary<string, Employee>> getEmployee()
+        {
+            return Ok(emp);
+        }
+
+
+    
+    }
+}
---
+++ EmployeePortal/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeePortal.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public int Exp { get; set; }
+    }
+}
